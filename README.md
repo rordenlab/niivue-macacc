9c445d6c97c79c53ea92f066f2218c06cf37443e
neurolabusc <rorden@sc.edu>
2025-03-05 09:51:11 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# niivue-macacc
-NiiVue viewer for MACACC Dataset
+### niivue-tinygrad
+
+This repository uses the WebGL2-based [NiiVue](https://github.com/niivue/niivue) to simulate the [acacc-viewer](https://brainbrowser.cbrain.mcgill.ca/macacc-viewer) of the seminal WebGL1 [BrainBrowser](https://github.com/aces/brainbrowser).
+
+### For Developers
+
+You can serve a hot-reloadable web page that allows you to interactively modify the source code.
+
+```bash
+git clone git clone git@github.com:rordenlab/niivue-macacc.git
+cd niivue-macacc
+npm install
+npm run dev
+```
+
+#### to build and serve the built version
+
+```bash
+npm run build
+npx http-server dist/ # or npm run preview
+```
+
---
+++ main.js
@@ -0,0 +1,92 @@
+import { Niivue } from '@niivue/niivue'
+
+async function main() {
+  aboutBtn.onclick = function () {
+    const { hostname, pathname } = window.location
+    const username = hostname.split('.')[0] // "niivue"
+    const repoName = pathname.split('/')[1] // "niivue-tinygrad"
+    const url = `https://github.com/${username}/${repoName}`
+    window.open(url, "_blank")
+  }
+  function doThreshChange() {
+    let mn = threshSlider.value * 0.1
+    let mx = mn + rangeSlider.value * 0.1
+    nv1.setMeshLayerProperty(nv1.meshes[0].id, 0, "cal_min", mn)
+    nv1.setMeshLayerProperty(nv1.meshes[0].id, 0, "cal_max", mx)
+  }
+  threshSlider.onchange = function () {
+    doThreshChange()
+  }
+  rangeSlider.onchange = function () {
+    doThreshChange()
+  }
+  modelSelect.onchange = async function () {
+    var meshLHLayersList1 = [
+      {
+        url: "./areaT_26163.mz3",
+        cal_min: 1.5,
+        cal_max: 5,
+        colormap: "warm",
+        opacity: 0.7,
+        colormapNegative: "",
+        useNegativeCmap: false,
+      },
+    ]
+    let mesh = './surf_reg_model_both_' + this.value + '.mz3'
+      const volumeObj = { url: mesh, layers: meshLHLayersList1 }
+      await nv1.loadMeshes([ volumeObj ])
+      doThreshChange()
+  }
+  colorSelect.onchange = function () {
+    let hex = this.value.slice(2)
+    let rgba = [0,0,0, 1]
+    rgba[0] = parseInt(hex.slice(0, 2), 16) / 255
+    rgba[1] = parseInt(hex.slice(2, 4), 16) / 255
+    rgba[2] = parseInt(hex.slice(4, 6), 16) / 255
+    nv1.opts.backColor = rgba
+    nv1.drawScene()
+  }
+  viewSelect.onchange = function () {
+    switch (this.selectedIndex) {
+      case 0:
+        nv1.setRenderAzimuthElevation(0, 0)
+        break
+      case 1:
+        nv1.setRenderAzimuthElevation(90, 0)
+        break
+      case 2:
+        nv1.setRenderAzimuthElevation(180, 0)
+        break
+      case 3:
+        nv1.setRenderAzimuthElevation(270, 0)
+        break
+      case 4:
+        nv1.setRenderAzimuthElevation(180, -90)
+        break
+      default:
+        nv1.setRenderAzimuthElevation(0, 90)
+    }
+  }
+  function handleLocationChange(data) {
+    document.getElementById("intensity").innerHTML = data.string
+  }
+  const defaults = {
+    onLocationChange: handleLocationChange,
+  }
+  const nv1 = new Niivue(defaults)
+  nv1.attachToCanvas(gl1)
+  nv1.opts.isColorbar = true
+  viewSelect.onchange()
+  let shaders = nv1.meshShaderNames()
+  for (let i = 0; i < shaders.length; i++) {
+    let btn = document.createElement("button")
+    btn.innerHTML = shaders[i]
+    btn.onclick = function () {
+      nv1.setMeshShader(nv1.meshes[0].id, shaders[i])
+    }
+    shaderBtns.appendChild(btn)
+  }
+  modelSelect.onchange()
+}
+
+main()
